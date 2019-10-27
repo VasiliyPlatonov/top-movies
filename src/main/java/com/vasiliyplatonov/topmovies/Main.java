4d043a87fd89353dc8c9ea1f2337309d361177bb
Vasiliy Platonov <vasiliyplat@gmail.com>
2019-10-27 19:26:59 +0300

    chore: add logback.xml and ignoring logs into gitignore

--- src/main/java/com/vasiliyplatonov/topmovies/Main.java
+++ src/main/java/com/vasiliyplatonov/topmovies/Main.java
@@ -1,16 +1,23 @@
 package com.vasiliyplatonov.topmovies;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 @ComponentScan
 public class Main {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        LOG.info("start app");
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(Main.class);
 
         context.close();
+        LOG.info("finish app");
     }
 }
