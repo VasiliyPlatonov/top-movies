c39177d3de34bb0bd2a44d04c80b3f46924e274a
Vasiliy Platonov <vasiliyplat@gmail.com>
2019-11-03 17:53:11 +0300

    feat: add top-movies-shell
     Added:
     - new module 'top-movies-shell' - feature to manage db server and
       fetcher from CLI
    
     Changes:
      - now by default the fetcher module stores the movie in a db file in
        the user's home directory

---
+++ top-movies-shell/src/main/java/com/vasiliyplatonov/topmovieshell/command/TopMoviesWebCommand.java
@@ -0,0 +1,23 @@
+package com.vasiliyplatonov.topmovieshell.command;
+
+import org.springframework.shell.standard.ShellComponent;
+
+@ShellComponent
+public class TopMoviesWebCommand {
+
+//    private final TopMoviesWeb topMoviesWeb;
+//
+//    @Autowired
+//    public TopMoviesWebCommand(TopMoviesWeb topMoviesWeb) {
+//        this.topMoviesWeb = topMoviesWeb;
+//    }
+//
+//    @ShellMethod("Start web-app ")
+//    public void webApp() {
+//        try {
+//            topMoviesWeb.start();
+//        } catch (LifecycleException e) {
+//            e.printStackTrace();
+//        }
+//    }
+}
---
+++ top-movies-shell/src/main/java/com/vasiliyplatonov/topmovieshell/service/LocalDateConverter.java
@@ -0,0 +1,35 @@
+package com.vasiliyplatonov.topmovieshell.service;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+@Service
+public class LocalDateConverter implements Converter<String, LocalDate> {
+    private static final Logger LOG = LoggerFactory.getLogger(LocalDateConverter.class);
+
+    @Nullable
+    @Override
+    public LocalDate convert(String date) {
+        LocalDate lclDate = null;
+
+        if (date.isEmpty()) {
+            lclDate = LocalDate.now();
+        } else {
+            try {
+                lclDate = LocalDate.parse(date);
+            } catch (DateTimeParseException e) {
+                LOG.info("Incorrectly top date entered. Correct text string such as 2007-22-03.");
+            }
+        }
+
+        return lclDate;
+    }
+}
